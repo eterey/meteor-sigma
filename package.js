791bb01df8ab705e83c80180fabc87b0e67eaa5a
maxim <uzumaxy@gmail.com>
2015-03-09 17:15:52 +0200

    Fixed path to plugins.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "uzumaxy:sigma",
-  summary: "Sigma is a library dedicated to graph drawing. It makes easy to publish networks on Web pages, and allows developers to integrate network exploration in rich Web applications.",
+  summary: "Sigma is a library dedicated to graph drawing.",
   version: "1.0.3",
   git: "https://github.com/uzumaxy/meteor-sigma"
 });
@@ -9,14 +9,14 @@ Package.onUse(function(api) {
 
   api.addFiles([
     "uzumaxy:sigma/sigma.min.js",
-    'sigma.layout.forceAtlas2.min.js',
-    'sigma.parsers.gexf.min.js',
-    'sigma.parsers.json.min.js',
-    'sigma.plugins.animate.min.js',
-    'sigma.plugins.dragNodes.min.js',
-    'sigma.plugins.filter.min.js',
-    'sigma.plugins.neighborhoods.min.js',
-    'sigma.renderers.customShapes.min.js',
-    'sigma.statistics.HITS.min.js'
+    "uzumaxy:sigma/plugins/sigma.layout.forceAtlas2.min.js",
+    "uzumaxy:sigma/plugins/sigma.parsers.gexf.min.js",
+    "uzumaxy:sigma/plugins/sigma.parsers.json.min.js",
+    "uzumaxy:sigma/plugins/sigma.plugins.animate.min.js",
+    "uzumaxy:sigma/plugins/sigma.plugins.dragNodes.min.js",
+    "uzumaxy:sigma/plugins/sigma.plugins.filter.min.js",
+    "uzumaxy:sigma/plugins/sigma.plugins.neighborhoods.min.js",
+    "uzumaxy:sigma/plugins/sigma.renderers.customShapes.min.js",
+    "uzumaxy:sigma/plugins/sigma.statistics.HITS.min.js"
   ], ["client"]);
 });
